e470fc2af6da6d66aaa8b000cbf14ef96603d489
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-11-29 16:53:16 +0600

    get users info based on their email and roles

--- index.js
+++ index.js
@@ -141,6 +141,14 @@ async function run() {
       res.send(result);
     })
 
+    app.get("/user/:email", async(req,res)=>{
+      const email = req.params.email;
+      // console.log(email);
+      const query = {email: email};
+      const result = await usersCollection.findOne(query);
+      res.send(result)
+    })
+
     app.patch("/user/:id", async(req,res)=>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
